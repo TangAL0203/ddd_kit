158c791e64a31977328092516458df850407321e
shengqintang <shengqintang@deeproute.ai>
2021-11-02 10:30:52 +0800

    update

--- visualization/load.py
+++ visualization/load.py
@@ -6,10 +6,10 @@ __all__ = ['load_pointcloud']
 
 def load_pointcloud(path):
     if path.endswith('bin'):
-        lidar_points = np.fromfile(str(path), dtype=np.float32).reshape(-1,4)
+        lidar_points = np.fromfile(str(path), dtype=np.float32).reshape(-1, 4)
     elif path.endswith('pcd'):
         scan = PyntCloud.from_file(str(path))
-        lidar_points = np.concatenate([scan.xyz, scan.points.intensity.values.reshape(-1,1)], axis=1)
+        lidar_points = np.concatenate([scan.xyz, scan.points.intensity.values.reshape(-1, 1)], axis=1)
     else:
         raise NotImplementedError()
     return lidar_points.astype(np.float32)
